a601c003084e30da5a02ff4843ea89575885041f
Marian Theisen <marian@cice-online.net>
2020-03-10 16:48:10 +0100

    first version

---
+++ index.ts
@@ -0,0 +1,53 @@
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import {map} from 'lodash-es';
+import {AppJson} from './lib/heroku';
+import Heroku = require('heroku-client');
+
+export interface FormationDyno {
+  size?: string;
+  quantity?: number;
+  type: string;
+  docker_image: string;
+}
+
+async function run() {
+  const herokuApiToken = core.getInput('heroku_api_token');
+  const appName = core.getInput('app');
+  const appJsonPath = core.getInput('app_json');
+  const imageRepository = core.getInput('image_repo');
+  const imageTag = core.getInput('image_tag');
+
+  const heroku = new Heroku({token: herokuApiToken});
+  const appJson: AppJson = JSON.parse(fs.readFileSync(appJsonPath).toString());
+
+  const dynos: FormationDyno[] = map(appJson.formation, (dynoDef, type) => {
+    return {
+      type,
+      ...dynoDef,
+      docker_image: `${imageRepository}/${type}:${imageTag}`,
+    };
+  });
+  dynos.push({
+    type: 'release',
+    quantity: undefined,
+    docker_image: `${imageRepository}/release:${imageTag}`,
+    size: undefined,
+  });
+
+  const formation = {
+    updates: dynos,
+  };
+
+  await heroku.patch(`/apps/${appName}/formation`, {body: formation});
+}
+
+try {
+  run().catch((e) => {
+    console.error(e);
+    core.setFailed(e.message);
+  });
+} catch (e) {
+  console.error(e);
+  core.setFailed(e.message);
+}
